0093ba3f2ec7527ef7ce4983bd7bb112797db028
Murilo Lima <chaveslimamurilo@gmail.com>
2025-12-02 22:22:40 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
-    return (
-        <>
-            <h1>Oi amor, eu te amo!❤️!!!!</h1>
-            <img src="https://i.pinimg.com/originals/a8/3a/90/a83a900ed250aa1ffd2e19ade3d4d926.gif"/>
-        </>
-    )
+  return (
+    <>
+      <h1>Oi amor, eu te amo!❤️!!!!</h1>
+      <img src="https://i.pinimg.com/originals/a8/3a/90/a83a900ed250aa1ffd2e19ade3d4d926.gif" />
+    </>
+  );
 }
